fad066f5de9f73468b6b254dc36b70ec1f9517f9
Joao Victor Farias <victorfarias.new@alu.ufc.br>
2020-04-08 18:45:43 -0300

    first and only commit

---
+++ README.md
@@ -0,0 +1,25 @@
+[![The MIT License](https://img.shields.io/badge/license-MIT-green.svg?style=flat-square)](LICENSE.md)
+
+## :rocket: Sobre o desafio
+
+Nesse desafio, é necessário criar um frontend que se comunique com a nossa api de repositórios de projeto.
+
+### Funcionalidades da aplicação
+
+Agora que você já está com o template clonado, e pronto para continuar, você deve abrir o arquivo **src/App.js**, e completar onde não possui código com o código para atingir os objetivos de cada funcionalidade.
+
+- **`Listar os repositórios da sua API`**: Deve ser capaz de criar uma lista com o campo **title** de todos os repositórios que estão cadastrados na sua API.
+
+- **`Adicionar um repositório a sua API`**: Deve ser capaz de adicionar um novo item na sua API através de um botão com o texto **Adicionar** e, após a criação, deve ser capaz de exibir o nome dele após o cadastro.
+
+- **`Remover um repositório da sua API`**: Para cada item da sua lista, deve possuir um botão com o texto **Remover** que, ao clicar, irá chamar uma função para remover esse item da lista do seu frontend e da sua API.
+
+## :memo: Licença
+
+Esse projeto está sob a licença MIT. Veja o arquivo [LICENSE](LICENSE.md) para mais detalhes.
+
+---
+
+<div align="center">
+Feito com 💜 by Joao Victor Farias :wave:
+</div>
--- src/App.js
+++ src/App.js
@@ -1,29 +1,76 @@
-import React from "react";
+import React, { useEffect, useState } from 'react';
 
-import "./styles.css";
+import api from './services/api';
+
+import './styles.css';
 
 function App() {
-  async function handleAddRepository() {
-    // TODO
+  const [repositories, setRepositories] = useState([]);
+  const [title, setTitle] = useState('');
+  const [url, setUrl] = useState('');
+  const [techs, setTechs] = useState([]);
+
+  useEffect(() => {
+    async function loadData() {
+      const response = await api.get('/repositories');
+      setRepositories(response.data);
+    }
+    loadData();
+  }, []);
+
+  async function handleAddRepository(e) {
+    e.preventDefault();
+    const response = await api.post('/repositories', {
+      title,
+      url,
+      techs,
+    });
+
+    setRepositories([...repositories, response.data]);
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    await api.delete(`/repositories/${id}`);
+
+    setRepositories(repositories.filter((repository) => repository.id !== id));
   }
 
   return (
     <div>
+      <form>
+        <h4>Adicione um novo repositório</h4>
+        <input
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          placeholder="Nome do repositório"
+        ></input>
+        <input
+          value={url}
+          onChange={(e) => setUrl(e.target.value)}
+          placeholder="URL do repositório"
+        ></input>
+        <input
+          value={techs}
+          onChange={(e) => setTechs(e.target.value)}
+          placeholder="Tecnologias"
+        ></input>
+        <button type="submit" onClick={handleAddRepository}>
+          Adicionar
+        </button>
+      </form>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+        {repositories.map((repository) => (
+          <li key={repository.id}>
+            <span>{repository.title}</span>
+            <button
+              type="button"
+              onClick={() => handleRemoveRepository(repository.id)}
+            >
+              Remover
+            </button>
+          </li>
+        ))}
       </ul>
-
-      <button onClick={handleAddRepository}>Adicionar</button>
     </div>
   );
 }
